b6fa17375e0d68fe2a085908d9e5b33ed7e68d68
Georgi Zlatev <gthebox@gmail.com>
2023-01-29 09:39:49 +0200

    Change background on "Win game"

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -8,10 +8,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No secretNumber!';
+
+    //When play win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When player to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'To high!';
@@ -21,6 +30,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Game over!🎆';
       document.querySelector('.score').textContent = 0;
     }
+
+    //When player to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'To low!';
