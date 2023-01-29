c1abd4504f37df319f2af8e6f166c11017a68fdd
Georgi Zlatev <gthebox@gmail.com>
2023-01-29 09:03:56 +0200

    add let score and reduction

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -13,7 +14,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number!';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'To high!';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'To low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
